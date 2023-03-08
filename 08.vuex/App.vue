51500ccea7ad52975df133a040b117c65ee728f1
YBogdan RADOI <radoi.office@gmail.com>
2023-03-08 16:45:16 +0100

    08.vuex - mutations, actions, updating state

--- 08.vuex/App.vue
+++ 08.vuex/App.vue
@@ -1,18 +1,49 @@
 <template>
-{{ store.state.count }}
+  <button v-for="post in posts" @click="click(post)"> 
+    {{ post.title }}
+  </button>
+  <h3>Id post cliqué : {{ postIdClicked ?? ' -- Pas de click pour le moment --' }}</h3>
+
+  <div v-if="currentPost">
+    <h1>{{ currentPost.title }}</h1>
+    <h4>{{ currentPost.content }}</h4>
+  </div>
 </template> 
 
 
 <!------------------------------- SCRIPT ------------------------------->
 <script>
+  import { computed, onMounted } from 'vue';
   import { useStore } from 'vuex';
 
   export default {
     setup() {
       const store = useStore();
-      // return variables to be used in template
+
+      const click = (post) => {
+        store.commit('setPostIdClicked', post.id);
+      }
+
+      const fetchData = () => {
+        store.dispatch('fetchPosts', 'additional info can be added here');
+      }
+
+      const currentPost = computed(() => {
+        return store.state.posts.find(x => {
+          return x.id === store.state.postIdClicked
+        })
+      })
+
+      // Called after component is rendered in the DOM
+      onMounted(() => {
+        fetchData();
+      })
+
       return {
-        store
+        postIdClicked: computed(() => store.state.postIdClicked),
+        posts: computed(() => store.state.posts),
+        click,
+        currentPost
       }
     }
   }
@@ -21,5 +52,17 @@
 
 <!-------------------------------- CSS --------------------------------->
 <style scoped>
+button {
+  width: 100px;
+  height: auto;
+  border: 2px solid #03ff03;
+  border-radius: 5px;
+  background-color: #fff;
+  color: red;
+  font-weight: 700;
+}
 
+button:hover {
+  background-color: #03ff03;
+}
 </style>
--- 08.vuex/store.js
+++ 08.vuex/store.js
@@ -1,10 +1,31 @@
 import { createStore } from "vuex";
+import { testPosts } from '../05.microblog/listPosts';
+
+const delay = () => new Promise(res => setTimeout(res, 1000));
 
 export const store = createStore({
   // similar to data() {return{}}
   state() {
     return {
-      count: 0
+      postIdClicked: null,
+      posts: []
+    }
+  },
+  // called with commit()
+  mutations: {
+    setPostIdClicked(state, postIdClicked) {
+      state.postIdClicked = postIdClicked
+    },
+    setPosts(state, posts) {
+      state.posts = posts;
+    }
+  },
+  // called with dispatch(), actions can be asynchronous
+  actions: {
+    async fetchPosts(ctx, payload) {
+      await delay();
+      console.log(payload);
+      ctx.commit('setPosts', testPosts)
     }
   }
 })
